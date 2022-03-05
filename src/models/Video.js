2913e7241c41910fd9656d8b194e7ae7f3c2e947
lastname-dev <lanto12@naver.com>
2022-03-06 06:52:46 +0900

    crud
    create
    read
    upload
    delete
    with mongodb

---
+++ src/models/Video.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const videoSchema = new mongoose.Schema({
+  title: { type: String, required: true, trim: true },
+  description: { type: String, required: true, trim: true },
+  createdAt: { type: Date, required: true, default: Date.now },
+  hashtags: [{ type: String, trim: true }],
+  meta: {
+    views: { type: Number, default: 0, required: true },
+    rating: { type: Number, default: 0, required: true },
+  },
+});
+
+videoSchema.static("formatHashtags", function (hashtags) {
+  return hashtags
+    .split(",")
+    .map((word) => (word.startsWith("#") ? word : `#${word}`));
+});
+
+videoSchema.pre("save", async function () {
+  console.log(this);
+});
+
+const Video = mongoose.model("Video", videoSchema);
+
+export default Video;
